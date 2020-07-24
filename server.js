07d033ea4b71765bdc37e8beea815c41f76e12c7
sahalan hasan <mythsahalan@yahoo.com>
2020-07-24 18:21:26 +0600

    connected to database

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+dotenv.config({ path: './config.env' });
 const app = require('./app');
 
 require('dotenv').config();
 
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(DB, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(con => {
+    console.log('DB connection succesfull');
+  });
 const Port = process.env.PORT | 3000;
 
 app.listen(Port, () => {
