b01d3ad3a698b367dd18fd7f8b1adac514915a25
sahalan hasan <mythsahalan@yahoo.com>
2020-07-26 10:41:16 +0600

    corrected sorting and added limiting and pagination

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -45,18 +45,41 @@ exports.getAllTours = async (req, res) => {
     console.log(JSON.parse(queryStr));
     let query = Tour.find(JSON.parse(queryStr));
 
-    // sorting
-    console.log(req.query);
+    // Sorting
+
     if (req.query.sort) {
       const sortBy = req.query.sort.split(',').join(' ');
       console.log(sortBy);
       query = query.sort(sortBy);
     } else {
-      query = query.sort(-createdAt);
+      query = query.sort('-createdAt');
+    }
+
+    // Limiting
+
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      console.log(fields);
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
+    // Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error("the page doesn't exist");
     }
 
     // execute query
     const tours = await query;
+    // const tours = await Tour.find();
 
     // send response
     res.status(201).json({
